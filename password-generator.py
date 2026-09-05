d6d5ab080e2323c6a5a2402cc5bc59bbfbd8254e
ericpreston <epreston984@gmail.com>
2026-09-04 23:39:21 -0400

    Added input validation for letters and special characters and additional comments.

--- password-generator.py
+++ password-generator.py
@@ -1,14 +1,17 @@
-# main task - have the program ask you the range of the password then output the result based on the answer. 
-# the output needs to include uppercase and lowercase letters, numbers, and special characters regardless.
-# the range will be 15 characters minimum. if the user asks for something lower or higher than 64 call it.
-# if a letter or special character is entered, then call it.
 import secrets
 import string
 
 print("This is the Password Generator! ")
 
-def generate_password(length = 25):
-    length = int(input("Enter the length of your password: "))
+# Main method
+def generate_password(length: int = 25):
+    # Catches entered letters and special characters for input.
+    try:
+        length = int(input("Enter the length of your password (15-64): "))
+    except ValueError:
+        print("You entered either a letter or special character. Please enter a number between 15 and 64.")
+        return
+    # Accounts for input length based on the entered integer.
     if length <= 14:
         print("This password is too short.")
         return
@@ -16,10 +19,12 @@ def generate_password(length = 25):
         print("This password is too long.")
         return
 
+    # Uses the imported libraries to call methods that compile all characters 
+    # Loops through the list to securely choose characters based on user input.
     full_list = string.ascii_letters + string.digits + string.punctuation
     password = ''.join(secrets.choice(full_list) for i in range(length))
     return password
 
-
+# Prints output
 password = generate_password()
-print(f"generated password: {password}")
+print(f"Generated password: {password}")
